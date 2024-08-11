086fe08d369f10bcbe794958e8b9b73a3e48e378
Varun <varunjethani@outlook.com>
2024-08-11 21:44:39 +0530

    update logout btn

--- src/components/header/logoutBtn.jsx
+++ src/components/header/logoutBtn.jsx
@@ -15,7 +15,7 @@ function LogoutBtn() {
     return (
         <button 
         onClick={logoutHandler}
-        className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'> 
+        className='inline-bock px-6 py-2 duration-200 hover:bg-red-600 rounded-full'> 
             Logout
         </button>
 
